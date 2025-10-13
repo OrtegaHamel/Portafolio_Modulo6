a6ebfdcb41b5b77da1c6af9250f3dd6d0d8dfabc
Ortega Hamel <ortega.hamel@gmail.com>
2025-10-13 18:54:36 -0300

    Implementación de formularios

--- mi_proyecto/mi_app/views.py
+++ mi_proyecto/mi_app/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductoForm
 from .models import Producto
 
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'mi_app/lista_productos.html', {'productos': productos})
 
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'mi_app/agregar_producto.html', {'form': form})
